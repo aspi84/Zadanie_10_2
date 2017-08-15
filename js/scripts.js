f77dd5819269a9f592cdfc7831c79df8e3a5e6eb
Agacia <agacia@gmail.com>
2017-08-15 17:18:43 +0200

    Changes in js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,17 @@ $(document).ready(function() {
 		  $(element).css('color', 'red');
 	  };
   });
-});
 
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
 
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
+  var paragraphs = $('p');
+  paragraphs.each(function(index, element) {
 
-});
+      var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+      $(element).append(button)
 
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+  });
+
+  $("button").click(function(){
+    alert($(this).attr("data-tmp"));
+  });
 });
